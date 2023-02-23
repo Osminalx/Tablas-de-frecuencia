ab79c1a846366064700d6174b3cca65b6eedcbbd
Osminalx <verdestro64@gmail.com>
2023-02-22 20:40:35 -0800

    Avanceeeeee

--- InputDeDatos.py
+++ InputDeDatos.py
@@ -1,14 +1,27 @@
 
 
-def Principal():
+def datosIniciales():
     numDatosTotal = int(input("ingresa la cantidad de datos: "))
-    numClases = round(pow(numDatosTotal, 1/2))
-    rango = 0
     datos = []
     for i in range(numDatosTotal):
         nuevoDato = float(input("ingresa un dato: "))
         datos.append(nuevoDato)
-    print(datos)
+    return datos, numDatosTotal
 
 
-Principal()
+def rango(datos: list):
+    min = datos[0]
+    max = datos[0]
+    for i in datos:
+        if i > max:
+            max = i
+        elif i < min:
+            min = i
+    rango = max - min
+    return rango, min, max
+
+
+def anchoDeClase(numDatosTotal: int, rango: float):
+    numClases = round(pow(numDatosTotal, 1/2))
+    anchoClase = rango//numClases
+    return anchoClase, numClases
