5b23e7ec626f04e813fef4a82ac1af33fed54f64
Osminalx <verdestro64@gmail.com>
2023-02-23 18:00:09 -0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA nosé

--- InputDeDatos.py
+++ InputDeDatos.py
@@ -1,27 +1,108 @@
 
 
-def datosIniciales():
-    numDatosTotal = int(input("ingresa la cantidad de datos: "))
+def datosIniciales(numDatosTotal: int):
     datos = []
     for i in range(numDatosTotal):
         nuevoDato = float(input("ingresa un dato: "))
         datos.append(nuevoDato)
-    return datos, numDatosTotal
+    return datos
 
 
-def rango(datos: list):
-    min = datos[0]
+def Max(datos: list):
     max = datos[0]
     for i in datos:
         if i > max:
             max = i
-        elif i < min:
+    return max
+
+
+def Min(datos: list):
+    min = datos[0]
+    for i in datos:
+        if i < min:
             min = i
-    rango = max - min
-    return rango, min, max
+    return min
 
 
-def anchoDeClase(numDatosTotal: int, rango: float):
-    numClases = round(pow(numDatosTotal, 1/2))
+def anchoDeClase(numClases, rango: float):
     anchoClase = rango//numClases
     return anchoClase, numClases
+
+
+def limitesInferiores(min: float, anchoClase: int):
+    limiteInf = []
+    limiteInf[0] = min-1
+    for i in range(1, 5):
+        limiteInf[i] = limiteInf[i-1]+anchoClase
+    return limiteInf
+
+
+def limitesSuperiores(limiteInf: list, anchoClase: int):
+    limiteSup = []
+    for i in limiteSup:
+        limiteSup[i] = limiteInf[i]+anchoClase
+    return limiteSup
+
+
+def Marcas(limiteInf: list, limiteSup: list):
+    marca = []
+    suma = 0
+    for i in limiteInf:
+        suma = limiteInf[i]+limiteSup[i]
+        marca[i] = suma/2
+        suma = 0
+    return marca
+
+
+def Frecuencias(datos: list, limiteInf: list, limiteSup: list):
+    cuenta = 0
+    frecuencia = []
+    for i in limiteInf:
+        for j in datos:
+            if datos[j] >= limiteInf[i] and datos[j] < limiteSup[i]:
+                cuenta += 1
+        frecuencia[i] = cuenta
+        cuenta = 0
+    return frecuencia
+
+
+def frecAcumulada(frecuencia: list):
+    frecuenAcumulada = []
+    frecuenAcumulada[0] = frecuencia[0]
+    for i in range(1, len(frecuencia)-1):
+        frecuenAcumulada[i] = frecuencia[i] + frecAcumulada[i-1]
+    return frecuenAcumulada
+
+
+def frecRelativa(numDatosTotal: int, frecuencia: list):
+    frecuenciaR = []
+    for i in frecuencia:
+        frecuenciaR[i] = frecuencia/numDatosTotal
+    return frecuenciaR
+
+
+def frecRelativaAcumulada(numDatosTotal: int, frecuenAcumulada: list):
+    frecuenciaRA = []
+    for i in frecuenAcumulada:
+        frecuenciaRA[i] = frecuenAcumulada/numDatosTotal
+    return frecuenciaRA
+
+
+def main():
+    numDatosTotal = int(input("ingresa la cantidad de datos: "))
+    primerTabla = datosIniciales(numDatosTotal)
+    maximo = Max(primerTabla)
+    minimo = Min(primerTabla)
+    rango = maximo - minimo
+    numClases = round(pow(numDatosTotal, 1/2))
+    anchClase = anchoDeClase(numClases, rango)
+    infLimit = limitesInferiores(minimo, anchClase)
+    upLimit = limitesSuperiores(infLimit, anchClase)
+    mark = Marcas(infLimit, upLimit)
+    frequency = Frecuencias(primerTabla, infLimit, upLimit)
+    accFrequency = frecAcumulada(frequency)
+    relFrequency = frecRelativa(numDatosTotal, frequency)
+    relAccFrequency = frecRelativaAcumulada(numDatosTotal, accFrequency)
+
+
+print(main())
